5c6bb7d0ef766947847f902c260e35532bbb7e88
Parzaan <parz.aa.an@gmail.com>
2026-06-22 15:14:41 +0530

    style: remove semicolons and fix unused import in renderer

--- app/src/renderer.js
+++ app/src/renderer.js
@@ -1,9 +1,9 @@
-import './index.css';
+import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  React.createElement(StrictMode, null, React.createElement(App, null))
+  React.createElement(React.StrictMode, null, React.createElement(App, null))
   // Renaming renderer.js to a jsx file is not an option as electron forge config looks specifically for renderer.js
 )
