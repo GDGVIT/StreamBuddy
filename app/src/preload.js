04d0a4750c5a36dd35ea47ef7029d2d574d93fd7
Parzaan <parz.aa.an@gmail.com>
2026-06-23 19:40:01 +0530

    feat: design dashboard UI and connect main process to python pipeline #5
    * style: Styled auth page
    
    * feat: design dashboard UI and connect main process to python pipeline
    
    * style: updated main and preload to comply with linter
    
    * style: updated indexcss to comply with linter

--- app/src/preload.js
+++ app/src/preload.js
@@ -1,2 +1,6 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from 'electron'
+
+contextBridge.exposeInMainWorld('electronAPI', { onStageUpdate: (callback) => ipcRenderer.on('stage-update', callback) })
